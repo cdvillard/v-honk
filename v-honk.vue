ba4bb607e49eb33d84b225c24b8c74fae74c47c9
Charles Villard <c.d.villard@gmail.com>
2019-10-04 11:02:56 -0400

    Restructure demo file

--- v-honk.vue
+++ v-honk.vue
@@ -1,44 +1,13 @@
 <template>
-  <a href="https://google.com" v-honk>Testing</a>
+  <a href="https://google.com" v-honk:block>Testing</a>
 </template>
 
 <script>
-const audio = new Audio(
-  "https://res.cloudinary.com/jlengstorf/video/upload/q_auto/v1569957993/honk-sound.mp3"
-);
-
-function honkify(event, arg, target) {
-  event.preventDefault();
-  event.stopImmediatePropagation();
-
-  // if the "goose" blocks
-  if (arg === "block") {
-    // honk at them and don't let them pass
-    audio.play();
-    return false;
-  } else {
-    // otherwise honk at them as they flee
-    setTimeout(() => {
-      window.location = target.href;
-    }, 300);
-    audio.play();
-  }
-}
+import honk from "./lib/index.js";
 
 export default {
   directives: {
-    honk: {
-      bind(el, binding) {
-        el.__HonkHandler__ = event => {
-          // passes event to prevent, arg if any, and target to send to
-          honkify(event, binding.arg, event.target);
-        };
-        document.body.addEventListener("click", el.__HonkHandler__);
-      },
-      unbind(el) {
-        document.body.removeEventListener("click", el.__HonkHandler__);
-      }
-    }
+    honk
   }
 };
 </script>
